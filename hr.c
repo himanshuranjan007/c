80f3fa17793240da105728242d83de7fa14baf47
himanshuranjan007 <155377613+himanshuranjan007@users.noreply.github.com>
2024-08-13 06:36:15 +0000

    add

--- hr.c
+++ hr.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
 
 int main(){
-	printf("PIZZZZZZZZZA");
+int firstNumber;
+int secondNumber;
+printf("ENTER first NUMBER :");
+scanf("%d",&firstNumber);
+printf("ENTER second NUMBER :");
+scanf("%d",&secondNumber);
+
+int result;
+result = firstNumber + secondNumber;
+printf("%d",result);
+
 }
